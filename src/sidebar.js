663ff5274befaf28b7d3faa9402337a022f9c9ad
FIRST_NAME LAST_NAME <vrushabhgawande63@gmail.com>
2021-05-04 23:15:16 +0530

    inital

--- src/sidebar.js
+++ src/sidebar.js
@@ -27,7 +27,7 @@ export default function Sid(){
 </div>
 
 <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+    <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
         <span>Components</span>
