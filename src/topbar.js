776af20f0d9dcfb23b650a2bf8ebacc1e469d1ab
FIRST_NAME LAST_NAME <vrushabhgawande63@gmail.com>
2021-05-04 23:52:44 +0530

    inital

--- src/topbar.js
+++ src/topbar.js
@@ -50,19 +50,19 @@ return <>
 
 
     <li class="nav-item dropdown no-arrow mx-1">
-        <a class="nav-link dropdown-toggle"  id="alertsDropdown" role="button"
+        <div class="nav-link dropdown-toggle" id="alertsDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-bell fa-fw"></i>
            
             <span class="badge badge-danger badge-counter">3+</span>
-        </a>
+        </div>
 
         <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="alertsDropdown">
             <h6 class="dropdown-header">
                 Alerts Center
             </h6>
-            <a class="dropdown-item d-flex align-items-center" >
+            <div class="dropdown-item d-flex align-items-center" >
                 <div class="mr-3">
                     <div class="icon-circle bg-primary">
                         <i class="fas fa-file-alt text-white"></i>
@@ -72,8 +72,8 @@ return <>
                     <div class="small text-gray-500">December 12, 2019</div>
                     <span class="font-weight-bold">A new monthly report is ready to download!</span>
                 </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" >
+            </div>
+            <div class="dropdown-item d-flex align-items-center" >
                 <div class="mr-3">
                     <div class="icon-circle bg-success">
                         <i class="fas fa-donate text-white"></i>
@@ -83,8 +83,8 @@ return <>
                     <div class="small text-gray-500">December 7, 2019</div>
                     $290.29 has been deposited into your account!
                 </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" >
+            </div>
+            <div class="dropdown-item d-flex align-items-center" >
                 <div class="mr-3">
                     <div class="icon-circle bg-warning">
                         <i class="fas fa-exclamation-triangle text-white"></i>
@@ -94,24 +94,24 @@ return <>
                     <div class="small text-gray-500">December 2, 2019</div>
                     Spending Alert: We've noticed unusually high spending for your account.
                 </div>
-            </a>
-            <a class="dropdown-item text-center small text-gray-500" >Show All Alerts</a>
+            </div>
+            <div class="dropdown-item text-center small text-gray-500" >Show All Alerts</div>
         </div>
     </li>
     <li class="nav-item dropdown no-arrow mx-1">
-        <a class="nav-link dropdown-toggle"  id="messagesDropdown" role="button"
+        <div class="nav-link dropdown-toggle"  id="messagesDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-envelope fa-fw"></i>
            
             <span class="badge badge-danger badge-counter">7</span>
-        </a>
+        </div>
        
         <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="messagesDropdown">
             <h6 class="dropdown-header">
                 Message Center
             </h6>
-            <a class="dropdown-item d-flex align-items-center">
+            <div class="dropdown-item d-flex align-items-center">
                 <div class="dropdown-list-image mr-3">
                     <img class="rounded-circle" src="img/undraw_profile_1.svg"
                         alt="..."/>
@@ -122,8 +122,8 @@ return <>
                         problem I've been having.</div>
                     <div class="small text-gray-500">Emily Fowler · 58m</div>
                 </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" >
+            </div>
+            <div class="dropdown-item d-flex align-items-center" >
                 <div class="dropdown-list-image mr-3">
                     <img class="rounded-circle" src="img/undraw_profile_2.svg"
                         alt="..."/>
@@ -134,8 +134,8 @@ return <>
                         would you like them sent to you?</div>
                     <div class="small text-gray-500">Jae Chun · 1d</div>
                 </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center">
+            </div>
+            <div class="dropdown-item d-flex align-items-center">
                 <div class="dropdown-list-image mr-3">
                     <img class="rounded-circle" src="img/undraw_profile_3.svg"
                         alt="..."/>
@@ -146,8 +146,8 @@ return <>
                         the progress so far, keep up the good work!</div>
                     <div class="small text-gray-500">Morgan Alvarez · 2d</div>
                 </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center">
+            </div>
+            <div class="dropdown-item d-flex align-items-center">
                 <div class="dropdown-list-image mr-3">
                     <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
                         alt="..."/>
@@ -158,8 +158,8 @@ return <>
                         told me that people say this to all dogs, even if they aren't good...</div>
                     <div class="small text-gray-500">Chicken the Dog · 2w</div>
                 </div>
-            </a>
-            <a class="dropdown-item text-center small text-gray-500">Read More Messages</a>
+            </div>
+            <div class="dropdown-item text-center small text-gray-500">Read More Messages</div>
         </div>
     </li>
 
@@ -167,32 +167,32 @@ return <>
 
     
     <li class="nav-item dropdown no-arrow">
-        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+        <div class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
             <img class="img-profile rounded-circle"
                 src="img/undraw_profile.svg"/>
-        </a>
+        </div>
       
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown">
-            <a class="dropdown-item">
+            <div class="dropdown-item">
                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                 Profile
-            </a>
-            <a class="dropdown-item" >
+            </div>
+            <div class="dropdown-item" >
                 <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                 Settings
-            </a>
-            <a class="dropdown-item">
+            </div>
+            <div class="dropdown-item">
                 <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                 Activity Log
-            </a>
+            </div>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" data-toggle="modal" data-target="#logoutModal">
+            <div class="dropdown-item" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                 Logout
-            </a>
+            </div>
         </div>
     </li>
 
