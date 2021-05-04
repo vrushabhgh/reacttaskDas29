a52bd443ff200543aef64837dcfc7a6dd307ed30
FIRST_NAME LAST_NAME <vrushabhgawande63@gmail.com>
2021-05-04 23:30:35 +0530

    inital

--- src/sidebar.js
+++ src/sidebar.js
@@ -27,11 +27,11 @@ export default function Sid(){
 </div>
 
 <li class="nav-item">
-    <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
+    <div class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
         <span>Components</span>
-    </a>
+    </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Components:</h6>
