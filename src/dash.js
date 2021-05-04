40919a458f37f3f3288bab5838eb7df485ace649
FIRST_NAME LAST_NAME <vrushabhgawande63@gmail.com>
2021-05-04 23:11:06 +0530

    inital

--- src/dash.js
+++ src/dash.js
@@ -4,8 +4,8 @@ export default function Dash() {
     return <>
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            <h3 class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-download fa-sm text-white-50"></i> Generate Report</h3>
         </div>
 
         <div class="row">
